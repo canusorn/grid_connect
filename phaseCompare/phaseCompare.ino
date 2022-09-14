bf6ec046b5887b6bbb5989581c0c450e9d105545
canusorn <anusorn1998@gmail.com>
2022-09-14 21:21:25 +0700

    add Vrms

--- phaseCompare/phaseCompare.ino
+++ phaseCompare/phaseCompare.ino
@@ -2,13 +2,15 @@ float min_v = 5, max_v;
 uint16_t minAnalog1 = 1023, maxAnalog1, minAnalog2 = 1023, maxAnalog2;
 uint32_t currentMillis, prevMillisTime1, prevMillisTime2;
 
-bool readFlag, periodFlag1, upFlag1;
+float f1_sum, f2_sum, f1, f2, f1_index, f2_index;
+bool readFlag, periodFlag1, upFlag1, periodFlag2, upFlag2;
 
-uint32_t analog1Sum, analog2Sum;
+uint32_t analog1Sum, analog2Sum, rms1Sum, rms2Sum;
 uint8_t analogSample;
 uint8_t cutState1, cutState2;
 
 uint16_t analog1, analog2, analog_av1 = 512, analog_av2 = 512;
+float analog1_rms, analog2_rms;
 
 void setup()
 {
@@ -45,10 +47,40 @@ void loop()
   {
     analog_av1 = analog1Sum / analogSample;
     analog_av2 = analog2Sum / analogSample;
+
+    analog1_rms = rms1Sum / analogSample;
+    analog1_rms = sqrt(analog1_rms);
+
+    analog2_rms = rms2Sum / analogSample;
+    analog2_rms = sqrt(analog2_rms);
+
     analog1Sum = 0;
     analog2Sum = 0;
+    rms1Sum=0;
+    rms2Sum=0;
     analogSample = 0;
-    // Serial.println(analog_av1);
+
+    f1 = f1_sum / f1_index;
+    f1_sum = 0;
+    f1_index = 0;
+    Serial.print("F1:" + String(f1));
+
+    f2 = f2_sum / f2_index;
+    f2_sum = 0;
+    f2_index = 0;
+    Serial.print("\tF2:" + String(f2));
+
+    float v1rms = analog1_rms * 5;
+    v1rms /= 1024;
+    Serial.print("\tVrms1:" + String(v1rms));
+
+    float v2rms = analog2_rms * 5;
+    v2rms /= 1024;
+    Serial.println("\tVrms2:" + String(v2rms));
+
+    float v1 = analog_av1 * 5;
+    v1 /= 1024;
+    // Serial.println("\tav1:" + String(v1, 2));
   }
 
   if (readFlag)
@@ -72,24 +104,63 @@ void loop()
       }
     }
 
+    if (upFlag2)
+    {
+      if (analog2 < analog_av2)
+      {
+        upFlag2 = 0;
+        cutState2++;
+      }
+    }
+    else
+    {
+      if (analog2 > analog_av2)
+      {
+        upFlag2 = 1;
+        cutState2++;
+      }
+    }
+
     if (cutState1 == 5)
     {
       cutState1 = 1;
       if (prevMillisTime1)
-          {
-            periodFlag1 = 1;
-            periodTime1 = currentMillis - prevMillisTime1;
-            periodTime1 /= 2;
-          }
-          prevMillisTime1 = currentMillis;
+      {
+        periodFlag1 = 1;
+        periodTime1 = currentMillis - prevMillisTime1;
+        periodTime1 /= 2;
+      }
+      prevMillisTime1 = currentMillis;
+    }
+
+    if (cutState2 == 5)
+    {
+      cutState2 = 1;
+      if (prevMillisTime1)
+      {
+        periodFlag2 = 1;
+        periodTime2 = currentMillis - prevMillisTime2;
+        periodTime2 /= 2;
+      }
+      prevMillisTime2 = currentMillis;
     }
   }
 
   if (periodFlag1)
   {
     periodFlag1 = 0;
-    Serial.print("T=" + String(periodTime1));
-    Serial.println("\tf=" + String(1000 / periodTime1));
+    // Serial.print("T=" + String(periodTime1));
+    // Serial.println("\tf=" + String(1000 / periodTime1));
+    f1_sum += 1000 / periodTime1;
+    f1_index++;
+  }
+  if (periodFlag2)
+  {
+    periodFlag2 = 0;
+    // Serial.print("T=" + String(periodTime1));
+    // Serial.println("\tf=" + String(1000 / periodTime1));
+    f2_sum += 1000 / periodTime2;
+    f2_index++;
   }
 }
 
@@ -102,8 +173,15 @@ ISR(TIMER1_COMPA_vect)
   currentMillis++;
   analogSample++;
   readFlag = 1;
+
   analog1 = analogRead(A0);
   analog1Sum += analog1;
+  uint16_t analog1_diff = abs(analog_av1 - analog1);
+  rms1Sum += analog1_diff * analog1_diff;
+
   analog2 = analogRead(A1);
   analog2Sum += analog2;
+  rms2Sum += analog2 * analog2;
+  uint16_t analog2_diff = abs(analog_av2 - analog2);
+  rms2Sum += analog2_diff * analog2_diff;
 }
